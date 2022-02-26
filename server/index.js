7b2002255d18878ed6f4add7254a41029d5bd956
Matt <77928207+mattzcarey@users.noreply.github.com>
2022-02-26 15:13:36 +0000

    image upload using multer

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-//Server code
-
-//imports
+//Server
+//import libraries
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const multer = require("multer");
+
+//define constants
 const app = express();
 const port = 3001;
 
@@ -12,16 +14,45 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//image storage
+const imageStorage = multer.diskStorage({
+  // Destination to store image
+  destination: (req, file, cb) => {
+    cb(null, "./images");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, (Date.now() + file.originalname).toString());
+  },
+});
+
+//image upload
+const imageUpload = multer({ storage: imageStorage });
+
+//use static folders
+app.use(express.static(__dirname + "/public"));
+app.use("/images", express.static("images"));
+
 //get method
 app.get("/", (req, res) => {
   res.send("Secret api stuff.");
 });
 
 //post method
-app.post("/image", (req, res) => {
-  const currentImage = req.body.image;
-  res.send("Posting image.");
-});
+app.post(
+  "/image",
+  imageUpload.single("image"),
+  (req, res) => {
+    console.log(JSON.stringify(req.file));
+    let response = '<a href="/">Home</a><br>';
+    response += "Files uploaded successfully.<br>";
+    response += `<img src="${req.file.path}" /><br>`;
+    return res.send(response);
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
