8c860191e3f0dd50490ff18f92bc553370c101ad
Matt <77928207+mattzcarey@users.noreply.github.com>
2022-02-26 12:33:46 +0000

    frontend timer

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    const difference = +new Date(`2022-02-27T11:45:00`) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [year] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+  return (
+    <div>
+      <h1>Hackathon February {year} Countdown</h1>
+      {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+    </div>
+  );
+}
+
+export default App;
